fc15d7b2d223a24f6bb9cec067f1f8a5c7709cf3
Ondrey <andrzejwrzesinski.2004@gmail.com>
2025-11-29 21:06:46 +0100

    Added BioGPT LLM model

---
+++ llms/biogpt_llm.py
@@ -0,0 +1,23 @@
+from interface.abstractmodel import AbstractModel
+from transformers import set_seed
+from transformers import BioGptTokenizer, BioGptForCasualLLM
+import torch
+
+class BioGPT(AbstractModel):
+    def generate_answer(self, prompt: str) -> str:
+        tokenizer = BioGptTokenizer.from_pretrained("microsoft/biogpt")
+        model = BioGptForCasualLLM.from_pretrained("microsoft/biogpt")
+		
+        inputs = tokenizer(prompt, return_tensors="pt")
+
+        set_seed(42)
+	
+        with torch.no_grad():
+            beam_output = model.generate(**inputs,
+                                         min_length=100,
+                                         max_length=1024,
+                                         num_beans=5,
+                                         early_stopping=True
+                                         )
+
+        return tokenizer.decode(beam_output[0], skip_special_tokens=True)
