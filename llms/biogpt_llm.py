0c5a0df2512a7e20650652226cdda989a8e52003
Andrzej Wrzesiński <andrzejwrzesinski.2004@gmail.com>
2025-11-30 00:04:09 +0100

    Added DeepSeekCoder LLM model

--- llms/biogpt_llm.py
+++ llms/biogpt_llm.py
@@ -7,16 +7,16 @@ class BioGPT(AbstractModel):
     def generate_answer(self, prompt: str) -> str:
         tokenizer = BioGptTokenizer.from_pretrained("microsoft/biogpt")
         model = BioGptForCausalLM.from_pretrained("microsoft/biogpt")
-		
+
         inputs = tokenizer(prompt, return_tensors="pt")
 
         set_seed(42)
-	
+
         with torch.no_grad():
             beam_output = model.generate(**inputs,
                                          min_length=100,
                                          max_length=1024,
-                                         num_beans=5,
+                                         num_beams=5,
                                          early_stopping=True
                                          )
 
