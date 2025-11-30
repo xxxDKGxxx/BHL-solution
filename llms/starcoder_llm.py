a6804904b32f9554f36e3d6c0b453f3f3a1a7df9
Andrzej Wrzesiński <andrzejwrzesinski.2004@gmail.com>
2025-11-30 11:00:56 +0100

    Modified llms

--- llms/starcoder_llm.py
+++ llms/starcoder_llm.py
@@ -1,17 +1,39 @@
 from interface.abstractmodel import AbstractModel
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
-class StarCoderForIT(AbstractModel):
+class QwenForIT(AbstractModel):
     def generate_answer(self, prompt: str) -> str:
-        tokenizer = AutoTokenizer.from_pretrained("bigcode/starcoderbase-1b")
-        model = AutoModelForCausalLM.from_pretrained("bigcode/starcoderbase-1b")
+        checkpoint = "Qwen/Qwen2.5-0.5B-Instruct"
 
-        inputs = tokenizer(prompt, return_tensors="pt")
+        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        model = AutoModelForCausalLM.from_pretrained(
+            checkpoint,
+            device_map="auto",
+            dtype=torch.float16
+        )
+
+        inputs = tokenizer(
+            prompt,
+            return_tensors="pt",
+            truncation=True
+        )
+
+        inputs = {k: v.to(model.device) for k, v in inputs.items()}
+
         outputs = model.generate(
-                                **inputs,
-                                max_new_tokens=200,
-                                no_repeat_ngram_size=3,
-                                eos_token_id=tokenizer.eos_token_id,
-                                pad_token_id=tokenizer.eos_token_id
-                                )
-        return tokenizer.decode(outputs[0], skip_special_tokens=True)
+            **inputs,
+            max_new_tokens=64,
+            min_length=20,
+            temperature=0.3,
+            top_p=0.8,
+            do_sample=True,
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.eos_token_id
+        )
+
+        return tokenizer.decode(
+            outputs[0],
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=True
+        )
