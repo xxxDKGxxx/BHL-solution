57ca225a0a773e0d04b6289aa7dea84c01982d35
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-01-22 10:51:05 +0100

    Remove unnecessary comma

--- train/reporting/model_interface.py
+++ train/reporting/model_interface.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Any, Optional, Self
 import numpy as np
 
 
-class ModelInterface(ABC, ):
+class ModelInterface(ABC):
 	"""
 	Abstrakcyjna klasa bazowa definiująca interfejs,
 	którego oczekuje klasa raportująca.
