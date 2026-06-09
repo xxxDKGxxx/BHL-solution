6b94863d90077f11bcc225d2b042730004a2b6f2
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-06-09 17:16:59 +0200

    Import fixes

--- app/llms/gemini_llm.py
+++ app/llms/gemini_llm.py
@@ -1,16 +1,17 @@
-from app.interface.abstractmodel import AbstractModel
+from interface.abstractmodel import AbstractModel
 import google.generativeai as genai
 
+
 class GeminiLLM(AbstractModel):
-	def __init__(self):
-		self.chat = None
+    def __init__(self):
+        self.chat = None
 
-	def generate_answer(self, prompt: str) -> str:
-		if self.chat is None:
-			api_key = ""
-			genai.configure(api_key=api_key)
-			model = genai.GenerativeModel('gemini-flash-latest')
-			self.chat = model.start_chat()
+    def generate_answer(self, prompt: str) -> str:
+        if self.chat is None:
+            api_key = ""
+            genai.configure(api_key=api_key)
+            model = genai.GenerativeModel("gemini-flash-latest")
+            self.chat = model.start_chat()
 
-		response = self.chat.send_message(prompt)
-		return response.text
+        response = self.chat.send_message(prompt)
+        return response.text
